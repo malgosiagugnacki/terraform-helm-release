883ec02ca7a70dc3b5bca4471a9ff1d5f13444fc
Malgosiagugnacki <malgosiagugnacki@gmail.com>
2025-04-18 21:43:37 +0000

    added module

---
+++ appmodule/varible.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "Please specify chart name"
+  type        = string
+  default     = "app"
+}
+
+variable "namespace" {
+  description = "Please specify chart namespace"
+  type        = string
+  default     = "app"
+}
+variable "values_yaml" {
+  description = "Please specify chart values"
+  type        = string
+  default     = ""
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-# Usage
-...Copy paste below cone and update the config
 module "helm-app" {
   source      = "./appmodule"
   name        = "terraform-helm-app"
   namespace   = "default"
   values_yaml = <<EOF
- 
+
+  
   replicaCount: 6
   
+
+
 image:
   repository: bitnami/wordpress
   pullPolicy: IfNotPresent
@@ -16,12 +17,3 @@ image:
 
   EOF 
 }
-
-```
-### Run the fallowing commands 
-
-```
-terraform init
-terraform apply
-```
-
